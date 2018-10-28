a495f2ce0b24cc907a22990a8aecbb5b1f86ba7f
Rahul Tiwari <jprrahultiwari@gmail.com>
2018-10-28 12:00:50 +0530

    Custom Bundle in GoalView

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,9 @@
 from .views import *
-#from . import url_converters
+# from . import url_converters
 from django.urls import register_converter, path
-app_name =  "main"
+
+app_name = "main"
 urlpatterns = [
 
-    path('ping',ping),
-    path('goals/new/',GoalView.as_view()),
+    path('goals/new/', GoalView.as_view()),
 ]
--- main/views.py
+++ main/views.py
@@ -14,22 +14,24 @@ class GoalView(APIView):
         pass
 
     def post(self, request):
-
         _type = request.data["_type"]
         amount = float(request.data["amount"])
         investment_amount = float(request.data["investment_amount"])
-        target_date = datetime.strptime(request.data["target_date"], "%d-%m-%Y").date() #dd-mm-YYYY
+        target_date = datetime.strptime(request.data["target_date"], "%d-%m-%Y").date()  # dd-mm-YYYY
         diff_years = (target_date - datetime.today().date()).days // 365
         print(diff_years)
         b_alloc, t_alloc = suggest_bundle(diff_years, investment_amount, amount)
 
         return JsonResponse(
             {
-                "bundle": {
-                    "returns": "8.5",
-                    "b_alloc": b_alloc,
-                    "t_alloc": t_alloc,
-                    "maturity_period": diff_years
-                }
+                "bundle": [
+                    {
+                        "returns": "8.5",
+                        "b_alloc": b_alloc,
+                        "t_alloc": t_alloc,
+                        "maturity_period": diff_years,
+                        "name": "Custom Bundle"
+                    }
+                ]
             }
         )
